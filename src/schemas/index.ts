deb7f8901ae1b656f51a7306189e73421b3b2ba9
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-09-19 14:35:57 +0000

    test: refactor test that ensures expected versions

---
+++ src/schemas/index.ts
@@ -0,0 +1,10 @@
+import * as types from '@nordicsemiconductor/lwm2m-types'
+
+// For these Objects we use the definition provided by @nordicsemiconductor/lwm2m-types
+export const ConnectivityMonitoring_4_urn = types.ConnectivityMonitoring_4_urn
+export const Device_3_urn = types.Device_3_urn
+export const Humidity_3304_urn = types.Humidity_3304_urn
+export const Location_6_urn = types.Location_6_urn
+export const parseURN = types.parseURN
+export const Pressure_3323_urn = types.Pressure_3323_urn
+export const Temperature_3303_urn = types.Temperature_3303_urn
